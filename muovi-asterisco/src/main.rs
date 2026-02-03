f3e82502ff232b1af20ef01f87f1a239ca34a5c8
Carlo Milanesi <carlo.milanesi@libero.it>
2026-02-03 11:44:28 +0000

    Add muovi-asterisco

---
+++ muovi-asterisco/src/main.rs
@@ -0,0 +1,38 @@
+fn main() -> std::io::Result<()> {
+    crossterm::terminal::enable_raw_mode()?;
+    crossterm::queue!(std::io::stdout(), crossterm::cursor::Hide)?;
+    let mut colonna_corrente = 0;
+    let numero_colonne = crossterm::terminal::size()?.0;
+    let numero_righe = crossterm::terminal::size()?.1;
+    loop {
+        crossterm::queue!(
+            std::io::stdout(),
+            crossterm::terminal::Clear(crossterm::terminal::ClearType::All),
+            crossterm::cursor::MoveTo(colonna_corrente, numero_righe - 1),
+            crossterm::style::Print('*'),
+        )?;
+        use std::io::Write;
+        std::io::stdout().flush()?;
+        match crossterm::event::read()? {
+            crossterm::event::Event::Key(tasto) => match tasto.code {
+                crossterm::event::KeyCode::Left => {
+                    if colonna_corrente > 0 {
+                        colonna_corrente -= 1;
+                    }
+                }
+                crossterm::event::KeyCode::Right => {
+                    if colonna_corrente < numero_colonne - 1 {
+                        colonna_corrente += 1;
+                    }
+                }
+                crossterm::event::KeyCode::Esc => break,
+                crossterm::event::KeyCode::Char('q') => break,
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+    crossterm::queue!(std::io::stdout(), crossterm::cursor::Show)?;
+    crossterm::terminal::disable_raw_mode()?;
+    Ok(())
+}
---
+++ somma2.sh
@@ -0,0 +1,5 @@
+echo -n "Primo addendo: "
+read addendo1
+echo -n "Secondo addendo: "
+read addendo2
+echo "La somma è" $((addendo1+addendo2))
