78eb472f69b3cb443c6cfc8ff8ea31de668f626a
Carlo Milanesi <carlo.milanesi@libero.it>
2026-02-20 09:14:27 +0000

    Update vari-errori

--- vari-errori/src/main.rs
+++ vari-errori/src/main.rs
@@ -1,16 +1,16 @@
-// //// 1. Non gestisce errori. ////
+//// 1. Non gestisce errori. ////
 
-fn leggi_e_stampa_numero() {
-    println!("Scrivi un numero: ");
-    let mut riga = String::new();
-    _ = std::io::stdin().read_line(&mut riga);
-    let numero = riga.trim().parse::<f64>().unwrap();
-    println!("Il numero formattato è {numero}.");
-}
+// fn leggi_e_stampa_numero() {
+//     println!("Scrivi un numero: ");
+//     let mut riga = String::new();
+//     _ = std::io::stdin().read_line(&mut riga);
+//     let numero = riga.trim().parse::<f64>().unwrap();
+//     println!("Il numero formattato è {numero}.");
+// }
 
-fn main() {
-    leggi_e_stampa_numero();
-}
+// fn main() {
+//     leggi_e_stampa_numero();
+// }
 
 //// 2. Gestisce errori di I/O, ma non errori di parsing di float. ////
 
@@ -51,14 +51,14 @@ fn main() {
 //// 4.a Gestisce sia errori di I/O, sia di parsing di float. ////
 //// Gestione errori caotica. ////
 
-// fn leggi_e_stampa_numero() -> Result<(), Box<dyn std::error::Error>> {
-//     println!("Scrivi un numero: ");
-//     let mut riga = String::new();
-//     _ = std::io::stdin().read_line(&mut riga)?;
-//     let numero = riga.trim().parse::<f64>()?;
-//     println!("Il numero formattato è {numero}.");
-//     Ok(())
-// }
+fn leggi_e_stampa_numero() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Scrivi un numero: ");
+    let mut riga = String::new();
+    _ = std::io::stdin().read_line(&mut riga)?;
+    let numero = riga.trim().parse::<f64>()?;
+    println!("Il numero formattato è {numero}.");
+    Ok(())
+}
 
 // fn main() {
 //     _ = leggi_e_stampa_numero().or_else(|err| {
@@ -80,15 +80,15 @@ fn main() {
 //// 4.b Gestisce sia errori di I/O, sia di parsing di float. ////
 //// Gestione errori ordinata, con uso di `is_some`. ////
 
-// fn main() {
-//     _ = leggi_e_stampa_numero().or_else(|err| {
-//         if err.downcast_ref::<std::num::ParseFloatError>().is_some() {
-//             eprintln!("Errore di parsing del numero: {err}");
-//         } else if err.downcast_ref::<std::io::Error>().is_some() {
-//             eprintln!("Errore di I/O: {err}");
-//         } else {
-//             eprintln!("Errore sconosciuto: {err}");
-//         }
-//         Err(err)
-//     });
-// }
+fn main() {
+    _ = leggi_e_stampa_numero().or_else(|err| {
+        if err.downcast_ref::<std::num::ParseFloatError>().is_some() {
+            eprintln!("Errore di parsing del numero: {err}");
+        } else if err.downcast_ref::<std::io::Error>().is_some() {
+            eprintln!("Errore di I/O: {err}");
+        } else {
+            eprintln!("Errore sconosciuto: {err}");
+        }
+        Err(err)
+    });
+}
